0cc86e917b57928e35194e3524c3fa0e063fb382
Franck Bushbaum <franck.bushbaum@yahoo.com>
2022-04-15 22:24:56 -0500

    k

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/small-circle-logo-only-v2.png'
+const Nav = () => {
+
+    const navigate = useNavigate();
+
+    return(
+        <>
+        <nav>
+            <div className="logo-container">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="controls-container">
+                <div className="icon" onClick={() => navigate('/ticket')}>TICKET</div>
+                <div className="icon" onClick={() => navigate('/')}>HOME</div>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Nav;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import TicketCard from "../components/TicketCard";
+
+const Dashboard = () => {
+    return(
+        <>
+        <div className="dashboard">
+            <h1>My Projects</h1>
+            <div className="ticket-container">
+                <TicketCard />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard;
